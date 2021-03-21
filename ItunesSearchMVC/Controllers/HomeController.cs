facb190430d5a3161bcd09ced536c7e47238cb20
Dan89-L <denillimbu18@hotmail.com>
2021-03-21 12:46:28 +0000

    Add Model, Read Data from API to Test

--- ItunesSearchMVC/Controllers/HomeController.cs
+++ ItunesSearchMVC/Controllers/HomeController.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using ItunesSearchMVC.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace ItunesSearchMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            string apiUrl = "https://itunes.apple.com/search?term=jack+johnson";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var searchContent = await response.Content.ReadAsStringAsync();
+
+                    var acutalData = Newtonsoft.Json.JsonConvert.DeserializeObject<ItunesSearchModel>(searchContent);
+                }
+            }
             return View();
         }
-
     }
 }
